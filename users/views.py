0ca2e6d48037ffa3c43f03ebec77b60745bce2d6
DESKTOP-MJ7UBM7\Jousset Nicolas <nicolas.jousset@etudiant.univ-rennes1.fr>
2020-12-18 14:16:35 +0100

    Hello world : with template

--- users/views.py
+++ users/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
+# Import
 from django.http import HttpResponse
+from django.shortcuts import render
+
+
+# Views
+def hello_old(request):
+    return HttpResponse("Hello Wolrd !")
 
 
 def hello(request):
-    return HttpResponse("Hello Wolrd !")
+    return render(
+        request,
+        'users/hello.html',
+        {
+            'message': "Hello Wolrd !",
+        }
+    )
